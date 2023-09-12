e7b00952c57588207336a179bf28b232a240f4d9
vangogh <itmannavys@gmail.com>
2023-09-12 10:06:04 +0700

    init file

---
+++ flaskr/auth.py
@@ -0,0 +1,19 @@
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from flaskr.db import get_db
+
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=['post', 'get'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        
